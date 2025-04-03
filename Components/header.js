2294db81248f566b2e37bd0d7cce4c51057aca83
Yusif892 <yk2385501@gmail.com>
2025-04-03 05:02:35 +0200

    Resolved admin header and user header

--- Components/header.js
+++ Components/header.js
@@ -1,5 +1,5 @@
 const header = {
-  getHTML: function () {
+  getAdminHeader: function () {
     return `<div class="logo">
         <img src="Imgs/Logo.png" alt="" />
         <h2>Books</h2>
@@ -13,7 +13,7 @@ const header = {
       </div>
 
       <ul class="nav-links">
-        <li><a href="#">Properties</a></li>
+        <li><a href="#">Favorites</a></li>
         <li><a href="#">Services</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">FAQ'S</a></li>
@@ -25,8 +25,42 @@ const header = {
         <button class="setting"><img src="Imgs/settings.png" /></button>
       </div>`;
   },
-  render: function (id) {
-    document.getElementById(id).innerHTML = this.getHTML();
+  getUserHeader: function () {
+    return `<div class="logo">
+        <img src="Imgs/Logo.png" alt="" />
+        <h2>Books</h2>
+      </div>
+
+      <div class="nav-side">
+        <div class="btns">
+          <a href="#" class="home">Home</a>
+        </div>
+
+        <ul class="nav-links">
+          <li><a href="#">Services</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact us</a></li>
+          <li><a href="#">Favourites</a></li>
+        </ul>
+      </div>
+
+      <div class="search-bar">
+        <img src="Imgs/search-normal.png" class="search-ic" />
+        <input
+          type="text"
+          placeholder="Search for title, author and category"
+        />
+      </div>
+
+      <div>
+        <button class="user"><img src="Imgs/user.png" /></button>
+      </div>`;
+  },
+  renderAdminHeader: function (id) {
+    document.getElementById(id).innerHTML = this.getAdminHeader();
+  },
+  renderUserHeader: function (id) {
+    document.getElementById(id).innerHTML = this.getUserHeader();
   },
 };
 
