5977275c81cedc6e02963c8f4b1e51cfe4a6aea7
Abdelrhman Ahmed <157153388+4abdelrhman@users.noreply.github.com>
2025-04-26 20:57:58 +0300

    Update header.js

--- Components/header.js
+++ Components/header.js
@@ -5,23 +5,33 @@ const header = {
           <h2>Books</h2>
         </div>
   
-        <div class="btns">
-          <a href="Books.html" class="home">Home</a>
-          <a href="#" class="exp"
-            >Explor <img src="Imgs/down-arrow.png" alt=""
-          /></a>
+        <div class="nav-side">
+          <div class="btns">
+            <a href="Books.html" class="home">Home</a>
+          </div>
+  
+          <ul class="nav-links">
+            <li><a href="#">Services</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact us</a></li>
+          </ul>
         </div>
   
-        <ul class="nav-links">
-        <li><a href="#">Services</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">FAQ'S</a></li>
-        <li><a href="#">Contact us</a></li>
-        </ul>
+        <div class="search-bar">
+          <img src="Imgs/search-normal.png" class="search-ic" />
+          <input
+            type="text"
+            placeholder="Search for title, author and category"
+            id="search-input"
+            autocomplete="off"
+          />
+        </div>
   
-        <div>
-          <a href="profile.html"><button class="user"><img src="Imgs/user.png" /></button></a>
-          <button class="setting"><img src="Imgs/settings.png" /></button>
+        <div class="profile-menu">
+          <button class="user"><img src="Imgs/user.png" /></button>
+          <div class="profile-menu-content">
+            <a href="logout.html">Logout</a>
+          </div>
         </div>`;
   },
   getUserHeader: function () {
@@ -48,18 +58,108 @@ const header = {
           <input
             type="text"
             placeholder="Search for title, author and category"
+            id="search-input"
           />
         </div>
   
-        <div>
-          <a href="profile.html"><button class="user"><img src="Imgs/user.png" /></button></a>
+        <div class="profile-menu">
+          <button class="user">
+          <img src="Imgs/user.png" />
+          </button>
+          <div class="profile-menu-content">
+            <a href="profile.html">view profile</a>
+            <a href="#" class="logout">Logout</a>
+          </div>
         </div>`;
   },
   renderAdminHeader: function (id) {
     document.getElementById(id).innerHTML = this.getAdminHeader();
+    this.adminSearchListener();
+    this.logOut();
   },
+
   renderUserHeader: function (id) {
     document.getElementById(id).innerHTML = this.getUserHeader();
+    this.userSearchListener();
+    this.logOut();
+  },
+
+  adminSearchListener: function () {
+    const searchInput = document.getElementById('search-input');
+    if (searchInput) {
+      searchInput.addEventListener('input', (event) => {
+        const searchVal = event.target.value.toLowerCase();
+        const books = JSON.parse(localStorage.getItem('books')) || [];
+        const container = document.querySelector('.books-container');
+        container.innerHTML = '';
+
+        const filteredBooks = books.filter((book) => {
+          return (
+            book.title.toLowerCase().includes(searchVal) ||
+            book.author.toLowerCase().includes(searchVal)
+          );
+        });
+
+        const booksToDisplay = searchVal ? filteredBooks : books;
+
+        booksToDisplay.forEach((book) => {
+          container.innerHTML += `<div class="book" data-id="${book.id}">
+            <img src="Imgs/${book.cover}.jpg" class="book-cover" />
+            <div class="book-side">
+              <div class="title">
+                <h2>${book.title}</h2>
+                <button class="edit-book" onclick="editBook(this)">
+                  <img src="Imgs/editing.png" class="edit-icon" />
+                </button>
+              </div>
+              <h4 class="auther">${book.author}</h4>
+              <p class="description">${book.description}</p>
+              <div class="id">
+                <p>ID:${book.id}</p>
+              </div>
+              <button class="remove" onclick="deleteBook(this)">Delete Book</button>
+            </div>
+          </div>`;
+        });
+      });
+    }
+  },
+
+  userSearchListener: function () {
+    const searchInput = document.getElementById('search-input');
+    if (searchInput) {
+      searchInput.addEventListener('input', (event) => {
+        const searchVal = event.target.value.toLowerCase();
+        const books = JSON.parse(localStorage.getItem('books')) || [];
+        const container = document.querySelector('.books-container');
+        container.innerHTML = '';
+
+        const filteredBooks = books.filter((book) => {
+          return (
+            book.title.toLowerCase().includes(searchVal) ||
+            book.author.toLowerCase().includes(searchVal)
+          );
+        });
+
+        const booksToDisplay = searchVal ? filteredBooks : books;
+
+        booksToDisplay.forEach((book) => {
+          container.innerHTML += ``;
+        });
+      });
+    }
+  },
+
+  logOut: function () {
+    const logout = document.querySelectorAll('.logout');
+
+    logout.forEach((link) => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+        localStorage.removeItem('username');
+        location.href = 'index.html';
+      });
+    });
   },
 };
 
